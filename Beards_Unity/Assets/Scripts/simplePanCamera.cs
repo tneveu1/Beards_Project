9d964a7e8755cb76ecb3df266f4e6674d8b169dd
Tim Neveu <tim.neveu@gmail.com>
2015-07-01 14:55:26 -0500

    Added prototype assets and scenes.  Also added the scripts folder

---
+++ Beards_Unity/Assets/Scripts/simplePanCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simplePanCamera : MonoBehaviour {
+
+	public float panSpeed = 10;
+	public float zoomInZ = 8;
+	public float zoomOutZ = 22;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				
+		
+		if (Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.up * panSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector3.left * panSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector3.right * panSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (Vector3.down * panSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.position = (new Vector3 (transform.position.x,transform.position.y,zoomInZ));
+		}
+		if (Input.GetKey (KeyCode.E)) {
+			transform.position = (new Vector3 (transform.position.x,transform.position.y,zoomOutZ));
+		}
+	}
+}
---
+++ Beards_Unity/Assets/Scripts/testCreate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testCreate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject instance = (GameObject)Instantiate(Resources.Load("Room_1x1_Center"));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
