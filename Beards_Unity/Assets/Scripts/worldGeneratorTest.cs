4b062eab7ef9503d345064c948d30e314cb2da36
racingspider <micah_neveu@hotmail.com>
2015-07-22 22:26:53 -0700

    A fine mess
    added new scene and new script… none of it does anything

---
+++ Beards_Unity/Assets/Scripts/worldGeneratorTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class worldGeneratorTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Object prefab = AssetBundle.CreateFromFile("Assets/Shaft.prefab");
+		//GameObject clone = Instantiate (prefab, Vector3.zero, Quaternion.identity) as GameObject;
+		//clone.transform.position = Vector3.one;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
